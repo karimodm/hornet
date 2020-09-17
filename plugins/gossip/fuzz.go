c4d666ab7686e031b3aa8291db102ddbdd9ddabe
Debian <admin@ip-172-31-1-53.eu-central-1.compute.internal>
2020-09-17 14:50:49 +0000

    Do not bias fuzzing if Tlverror

--- plugins/gossip/fuzz.go
+++ plugins/gossip/fuzz.go
@@ -5,8 +5,11 @@ import "github.com/gohornet/hornet/pkg/protocol"
 func Fuzz(data []byte) int {
 	p := protocol.New(nil)
 	p.Receive(data)
+	/*
 	if p.Tlverror {
 		return 0
 	}
 	return 1
+	*/
+	return 0
 }
